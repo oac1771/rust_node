c471f3f978c9f5b3104837b980612a6a89ddbdea
Omar Carey <omarcarey@omars-mbp.lan>
2023-04-10 16:48:55 -0500

    add id endpoint

--- src/client/request_client.rs
+++ src/client/request_client.rs
@@ -1,79 +1,80 @@
-// use std::collections::HashMap;
-// use futures::{future::BoxFuture, FutureExt};
+use std::collections::HashMap;
+use futures::{future::BoxFuture, FutureExt};
 
 use reqwest;
 use rocket::serde::Serialize;
 
-// look into having http methods in client take a struct as deserializer?
-// 
-
-// pub struct RequestClient {
-//     client: reqwest::Client
-
-// }
-
-// impl RequestClient {
-//     pub async fn post(&self, data: HashMap<&str, &str>, url: &str) -> Response{
-//         let request =  || async move {self.client.post(url).json(&data).send().await}.boxed();
-//         let response = self.call(request).await;
-
-//         return response
-
-//     }
-
-//     // see if there is a way to make this impl thing more compact, maybe using where?
-//     async fn call<'a>(&self, request: impl FnOnce() -> BoxFuture<'a, Result<reqwest::Response, reqwest::Error>>) -> Response {
-//         let r = request().await.unwrap();
-
-//         let response = Response::new(r);
-
-//         return response
-
-//             // todo: verify that response from /receive endpoint is being matched to match thing inside this
-//         // you have to wrap response from reqwest in custom struct/enum then create service to be able
-//         // to process. In forward something that handles and sends reqwuest post; in receive something that handles
-//         // and reqwuest and returns correct response
-
-//         // match response {
-//         //     Ok(response) => {
-//         //         match response.status() {
-//         //             reqwest::StatusCode::OK => {
-//         //                 println!("status code: {}", response.status());
-//         //             },
-//         //             _ => {
-//         //                 println!("foo")
-//         //             }
-//         //         }
-
-//         //     },
-//         //     Err(_error) => {
-//         //         println!("Uh oh! Something unexpected happened.");
-//         //     },
-//         // }
-//     }
-
-
-//     pub fn new() -> RequestClient {
-//         let client = reqwest::Client::new();
-//         let request_client = RequestClient {
-//             client
-//         };
-
-//         return request_client
-//     }
-// }
 
 #[derive(Serialize)]
 pub struct Response {
     pub status_code: String,
+    pub body: serde_json::Value
 }
 
 impl Response {
-    fn new(r: reqwest::Response) -> Response {
+    async fn new(r: reqwest::Response) -> Response {
+
         let response = Response {
-            status_code: r.status().to_string()
+            status_code: r.status().to_string(),
+            body: r.json::<serde_json::Value>().await.unwrap()
         };
 
         return response
     }
+}
+
+pub struct RequestClient {
+    client: reqwest::Client
+
+}
+
+impl RequestClient {
+
+    pub fn new() -> RequestClient {
+        let client = reqwest::Client::new();
+        let request_client = RequestClient {
+            client
+        };
+
+        return request_client
+    }
+
+    pub async fn post(&self, data: HashMap<&str, &str>, url: &str) -> Response {
+        let request =  || async move {self.client.post(url).json(&data).send().await}.boxed();
+        let response = self.call(request).await;
+
+        return response
+
+    }
+
+    // see if there is a way to make this impl thing more compact, maybe using where?
+    async fn call<'a>(&self, request: impl FnOnce() -> BoxFuture<'a, Result<reqwest::Response, reqwest::Error>>) -> Response {
+        let r = request().await.unwrap();
+        let response = Response::new(r);
+
+        return response.await
+
+            // todo: verify that response from /receive endpoint is being matched to match thing inside this
+        // you have to wrap response from reqwest in custom struct/enum then create service to be able
+        // to process. In forward something that handles and sends reqwuest post; in receive something that handles
+        // and reqwuest and returns correct response
+
+        // match response {
+        //     Ok(response) => {
+        //         match response.status() {
+        //             reqwest::StatusCode::OK => {
+        //                 println!("status code: {}", response.status());
+        //             },
+        //             _ => {
+        //                 println!("foo")
+        //             }
+        //         }
+
+        //     },
+        //     Err(_error) => {
+        //         println!("Uh oh! Something unexpected happened.");
+        //     },
+        // }
+    }
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[macro_use] extern crate rocket;
 use rocket::serde::json::Json;
 use reqwest;
@@ -8,10 +10,26 @@ mod client;
 fn health() -> Json<client::request_client::Response> {
 
     Json(client::request_client::Response{
-        status_code: reqwest::StatusCode::OK.to_string()
+        status_code: reqwest::StatusCode::OK.to_string(), 
+        body: serde_json::json!("")
     })
 }
+
+#[get("/add")]
+fn add() {
+
+}
+
+#[post("/id")]
+async fn id() -> Json<client::request_client::Response> {
+    let data: HashMap<&str, &str> = HashMap::new();
+    let client = client::request_client::RequestClient::new();
+    let response = client.post(data, "http://127.0.0.1:5001/api/v0/id").await;
+
+    return Json(response)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![health])
+    rocket::build().mount("/", routes![health, add, id])
 }
