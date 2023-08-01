862655d91f90077f75b834d2f450ce804e36fff9
Omar Carey <omarcarey@omars-mbp.lan>
2023-08-01 00:20:42 -0500

    add checking for event in identifier test

--- contract/contracts/Identifier.sol
+++ contract/contracts/Identifier.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Identifier is ERC721 {
 
@@ -10,16 +10,37 @@ contract Identifier is ERC721 {
 
     event AuthenticationRequest(string indexed ipfsAddress);
 
-    mapping(uint256 => string) private nftToIpfs;
+    mapping(uint256 => string) public tokenIdToIpfs;
+    mapping(uint256 => string) public tokenIdToPrincipal;
 
     constructor() ERC721("Identity Token", "IDTKN") {
         tokenId = 0;
     }
 
-    function registerIdentity(address principal, string memory ipfsAddress) external {
+    modifier onlyTokenOwner(uint256 tokenId) {
+        require(ownerOf(tokenId) == msg.sender, "You are not the owner of this token");
+        _;
+    }
+
+    function registerIdentity(address principal, string memory ipfsAddress) 
+    external {
         _safeMint(principal, tokenId);
-        nftToIpfs[tokenId] = ipfsAddress;
+        tokenIdToIpfs[tokenId] = ipfsAddress;
 
         tokenId++;
+    }
+
+    function getIpfsAddress(uint256 tokenId) 
+    view public returns(string memory) {
+        return tokenIdToIpfs[tokenId];
+    }
+
+    function getCurrentTokenID() view public returns(uint256) {
+        return tokenId;
+    }
+
+    function authenticate(uint256 tokenId) 
+    public onlyTokenOwner(tokenId) returns(string memory){
+        return "hi";
     }
 }
--- contract/test/main.test.ts
+++ contract/test/main.test.ts
@@ -3,9 +3,20 @@ import { Wallet, Provider, Contract } from 'zksync-web3';
 import * as hre from 'hardhat';
 import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
 
-const RICH_WALLET_PK =
-  '0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110';
-const PRINCIPAL = "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049"
+const DEPLOYER_CREDS = {
+  "privateKey": '0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110',
+  "address": "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049"
+}
+
+const PRINCIPAL_CREDS = {
+  "privateKey": "0xd293c684d884d56f8d6abd64fc76757d3664904e309a0645baf8522ab6366d9e",
+  "address": "0x0D43eB5B8a47bA8900d84AA36656c92024e9772e"
+}
+
+// const RICH_WALLET_PK =
+//   '0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110';
+// const PRINCIPAL = "0x0D43eB5B8a47bA8900d84AA36656c92024e9772e"
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 
 async function deployIdentifier(deployer: Deployer): Promise<Contract> {
@@ -17,19 +28,20 @@ describe('Identifier', function () {
   it("Should emit transfer event when new identity is registered", async function () {
     const provider = Provider.getDefaultProvider();
 
-    const wallet = new Wallet(RICH_WALLET_PK, provider);
+    const wallet = new Wallet(DEPLOYER_CREDS["privateKey"], provider);
     const deployer = new Deployer(hre, wallet);
+
+    const ipfsAddress = "https://ipfs.io/ipfs/Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu"
 
     const identifier = await deployIdentifier(deployer);
 
-    const ipfsAddress = "ipfsAddress"
-
-    await expect(await identifier.registerIdentity(PRINCIPAL, ipfsAddress)).to.emit(identifier, 'Transfer');
-
-    // const setGreetingTx = await greeter.setGreeting('Hola, mundo!');
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await greeter.greet()).to.equal('Hola, mundo!');
+    await expect(await identifier.registerIdentity(PRINCIPAL_CREDS["address"], ipfsAddress))
+      .to.emit(identifier, 'Transfer')
+      .withArgs(ZERO_ADDRESS, PRINCIPAL_CREDS["address"], 0);
+    
+    const ipfsAddressFromContract = await identifier.getIpfsAddress(0)
+    
+    expect(ipfsAddressFromContract).to.equal(ipfsAddress)
+    expect(await identifier.getCurrentTokenID()).to.equal(1)
   });
 });
